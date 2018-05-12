9a9d2fcb846d7ef5bb30d6d45522b5d1aa4a655b
Dor Tzur <dortzur@gmail.com>
2018-05-12 10:13:44 +0300

    delete backpack config

--- backpack.config.js
+++
@@ -1,9 +0,0 @@
-// backpack.config.js
-module.exports = {
-  webpack: (config, options, webpack) => {
-    console.log('CONIFG', config, options);
-    // Perform customizations to config
-    // Important: return the modified config
-    return config;
-  }
-};
